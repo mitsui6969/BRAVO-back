c225216c80939f0f70981d0eac195cac812f9f92
nagisa <136070735+mitsui6969@users.noreply.github.com>
2024-10-17 00:00:18 +0900

    フロントとの繋ぎ完了

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # CORS設定を追加
 
 @app.route('/')
 def hello():
+    print("api送れたよー!")
     return "Hello from Flask!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
